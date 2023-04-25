d152c3a161e4e132b4b06f45faedb04abdb8d7b2
dz_paji <19299425+dz-paji@users.noreply.github.com>
2023-04-25 01:06:58 +0100

    Update Eval.hs

--- Eval.hs
+++ Eval.hs
@@ -1,10 +1,15 @@
 module Eval where import TileGrammar
 
+type CEK = (Exp, Env, Kont)
+type Env = [(String, TileInt)]
+type Frame = Exp [-] | [-] Exp
+type Kont = [Frame]
+
 -- evalExp, evaluates an exp
 -- TODO: standardize the return type of evalExp
 evalExp expr = case expr of
     (CreateCanvas var calc_expr) -> n
-    (Load var) -> (var, var)
+    (Load var) -> (var, var) // 导入文件
     (Reverse var) -> reverse var
     (Rotate var calc_expr) -> rotateTile var calc_expr
     (Blank var) -> (var, []) -- 这又是干啥的来着
@@ -13,7 +18,7 @@ evalExp expr = case expr of
     (Subtitle var_a var_b) -> var -- 这是干啥的来着
     (IfThen bool_expr then_expr) -> evalIfThen (evalBoolExpr bool_expr) then_expr
     (IfElse bool_expr then_expr else_expr) -> evalIfElse (evalBoolExpr bool_expr) then_expr else_expr
-    (Let var calc_expr) -> var
+    (Assign var calc_expr) -> var
 
 -- TODO: implement rotateTile
 rotateTile :: Exp -> CalcExpr -> Tile
@@ -51,3 +56,10 @@ evalCalc expr = case expr of
     (PlusOne a) -> (a + 1)
     (Int a) -> a
     (Get a) -> (show a)
+
+letsEval :: Exp -> Exp
+letsEval e = doEval (e, [], [])
+
+doEval (e, env, kon) =  
+    where
+
